8718adbaef9980ddb6aa3fe8aa9c85b74030170d
Johannes Siipola <johannes@siipo.la>
2015-10-27 19:18:07 +0200

    Form to add new boards

--- index.php
+++ index.php
@@ -19,7 +19,33 @@ $capsule->addConnection([
   'charset' => 'utf8',
   'collation' => 'utf8_unicode_ci',
 ]);
+
 $capsule->bootEloquent();
 $capsule->setAsGlobal();
+$container = $app->getContainer();
+$container['generateHash'] = function (){
+    $factory = new RandomLib\Factory;
+    $generator = $factory->getMediumStrengthGenerator();
+    return $generator->generateString(8, 'abcdefghijklmnopqrstuvwxyz');
+};
+
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('templates');
+    $view->addExtension(new Slim\Views\TwigExtension(
+      $c['router'],
+      $c['request']->getUri()
+    ));
+    return $view;
+};
+
+$app->map(['GET', 'POST'], '/add-board', function(\Slim\Http\Request $req, \Slim\Http\Response $res, $args){
+    $this->view->render($res, 'add-board.twig');
+    if ($req->isPost()){
+        $board = new \Sprintboard\Model\Board();
+        $board->name = $req->getParam('name');
+        $board->hash = $this->generateHash;
+        $board->save();
+    }
+});
 
 $app->run();

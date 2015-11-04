4039212615d5a7b1ed58b980c4855f0e48f681b5
Johannes Siipola <johannes@siipo.la>
2015-11-04 22:52:18 +0200

    Build system for bootstrap

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var app = angular.module('sprintBoard', []);
 app.controller('boardController', function($scope, $http) {
     var promise = $http.get(API_ROOT + 'board/' + BOARD_HASH);
     promise.then(function(data){
-        $scope.cards = data.data.cards;
+        $scope.board = data.data;
         console.log(data.data.cards);
     })
 });

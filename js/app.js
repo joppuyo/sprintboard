486aa02daecf1c488fb16b9c8f17d0c1dbb03693
Johannes Siipola <johannes@siipo.la>
2015-11-16 20:11:39 +0200

    Update mark as done

--- js/app.js
+++ js/app.js
@@ -77,9 +77,9 @@ app.controller('taskController', function($scope, $http, $rootScope, $uibModal){
     $scope.markAsDone = function() {
         var promise;
         if ($scope.task.is_done) {
-            promise = $http.delete(API_ROOT + 'board/' + BOARD_HASH + '/card/' + $scope.card.id + '/task/' + $scope.task.id + '/done');
+            promise = $http.delete(API_ROOT + 'task/' + $scope.task.id + '/done');
         } else {
-            promise = $http.put(API_ROOT + 'board/' + BOARD_HASH + '/card/' + $scope.card.id + '/task/' + $scope.task.id + '/done');
+            promise = $http.put(API_ROOT + 'task/' + $scope.task.id + '/done');
         }
         promise.then(function(){
             $rootScope.$broadcast('boardUpdateEvent');
